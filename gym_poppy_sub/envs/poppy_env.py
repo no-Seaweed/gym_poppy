7741b3c1cf5bdf0f9fd471f231c1b8c6696f1fbd
no-Seaweed <40819442+no-Seaweed@users.noreply.github.com>
2019-04-18 14:37:31 -0400

    Update poppy_env.py

--- gym_poppy_sub/envs/poppy_env.py
+++ gym_poppy_sub/envs/poppy_env.py
@@ -3,19 +3,27 @@
 """
 Created on Wed Apr 17 22:01:10 2019
 
-@author: macbook
+@author: Shukai
 """
 
+import logging.config
 import gym
+import cfg_load
 from gym import error, spaces, utils
 from gym.utils import seeding
+import pkg_resources
+
+path = 'config.yaml'  # always use slash in packages
+filepath = pkg_resources.resource_filename('gym_banana', path)
+config = cfg_load.load(filepath)
+logging.config.dictConfig(config['LOGGING'])
 
 class PoppyEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
   def __init__(self):
     self.__version__ = "0.1.0"
-    logging.info("BananaEnv - Version {}".format(self.__version__))
+    logging.info("hello")
     self.MAX_PRICE = 2.0
   def step(self, action):
     ...
@@ -25,3 +33,4 @@ class PoppyEnv(gym.Env):
     ...
   def close(self):
     ...
+
